ee766489222ea8de285a2d1a2303dce7848c74ca
parag-0501 <parag0501ds@gmail.com>
2023-08-18 13:38:49 +0530

    5th ans

---
+++ HandsOn1/5/Create5Files.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<fcntl.h>
+
+int main() {
+	int file1 = creat("file1.txt",0644);
+	int file2 = creat("file2.txt",0644);
+	int file3 = creat("file3.txt",0644);
+	int file4 = creat("file4.txt",0644);
+	int file5 = creat("file5.txt",0644);
+	while(1);
+}
+
