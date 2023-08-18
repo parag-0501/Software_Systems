934f810896edd3d6575ab2f8ae9543adb9b57c9f
parag-0501 <parag0501ds@gmail.com>
2023-08-18 13:32:14 +0530

    4th ans

---
+++ HandsOn1/4/OpenFile_O_EXCL.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main() {
+	const char *file_name = "example.txt";
+	int file_descriptor = open(file_name, O_RDWR);
+
+	if(file_descriptor == -1) {
+		perror("Open Failed");
+		return -1;
+	} 
+	else {
+		printf("File '%s' opened in read-write mode with file descriptor: %d\n", file_name, file_descriptor);
+	}
+
+	int file_descriptor_excl = open(file_name, O_RDWR | O_EXCL);
+
+	if(file_descriptor_excl == -1) {
+		perror("open with )_EXCL");
+		printf("O_EXCL flag failed as expected. \n");
+	} 
+	else {
+		printf("O_EXCL flag opened the file with file descriptor: %d\n", file_descriptor_excl);
+		close(file_descriptor_excl);
+	}
+
+	if(close(file_descriptor) == -1) {
+		perror("Close Failed");
+		return 1;
+	}
+
+	return 0;
+}
