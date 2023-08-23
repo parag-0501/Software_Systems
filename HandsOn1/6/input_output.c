2e6b029ae0790b3190c146b50030848bea1d43ee
parag-0501 <parag0501ds@gmail.com>
2023-08-23 22:13:27 +0530

    6th ans

--- HandsOn1/6/input_output.c
+++ HandsOn1/6/input_output.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main() {
-	char s[1024];
+    char buffer[1024];  // Buffer to hold input
 
-	scanf("%s", s);
-	printf("%s", s);
+    size_t bytesRead;
+    while ( (bytesRead = read( STDIN_FILENO, buffer, sizeof(buffer) )) > 1 ) {
+        // Write the contents of the buffer to STDOUT
+        write(STDOUT_FILENO, buffer, bytesRead);
+    }
 
-	return 0;
+    if (bytesRead == -1) {
+        perror("Error reading from STDIN");
+        return 1;
+    }
+
+    return 0;
 }
