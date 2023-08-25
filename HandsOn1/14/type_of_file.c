6c3533d36fbbde23e8af1d32188641e9df2ab48c
parag-0501 <parag0501ds@gmail.com>
2023-08-25 09:46:18 +0530

    Find_type_of_file

---
+++ HandsOn1/14/type_of_file.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    char *filename = argv[1];
+    struct stat fileStat;
+
+    if (stat(filename, &fileStat) == -1) {
+        printf("Error getting file information.\n");
+        return 1;
+    }
+
+    if (S_ISREG(fileStat.st_mode)) {
+        printf("%s: Regular file\n", filename);
+    } else if (S_ISDIR(fileStat.st_mode)) {
+        printf("%s: Directory\n", filename);
+    } else if (S_ISLNK(fileStat.st_mode)) {
+        printf("%s: Symbolic link\n", filename);
+    } else if (S_ISFIFO(fileStat.st_mode)) {
+        printf("%s: FIFO (named pipe)\n", filename);
+    } else if (S_ISCHR(fileStat.st_mode)) {
+        printf("%s: Character special file\n", filename);
+    } else if (S_ISBLK(fileStat.st_mode)) {
+        printf("%s: Block special file\n", filename);
+    } else {
+        printf("%s: Unknown file type\n", filename);
+    }
+
+    return 0;
+}
