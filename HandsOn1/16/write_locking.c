5520863e925199a2afd7d42fc0339505f451ff58
parag-0501 <parag0501ds@gmail.com>
2023-08-25 10:19:38 +0530

    Locking_with_write_and_read

---
+++ HandsOn1/16/write_locking.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+	struct flock lock;
+	int fd;
+	fd = open("db" , O_RDWR);
+	lock.l_type = F_WRLCK;
+	lock.l_whence = SEEK_SET;
+	lock.l_start = 0;
+	lock.l_len = 0;
+	lock.l_pid = getpid();
+	printf("Before Entering Critical Section...\n");
+	fcntl(fd, F_SETLKW, &lock);
+	printf("Inside the critical section...\n");
+	printf("Enter to unlock...\n");
+	getchar();
+	printf("Unlocked...\n");
+	lock.l_type = F_UNLCK;
+	fcntl(fd, F_SETLK, &lock);
+	printf("Finished...\n");
+}
