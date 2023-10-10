52280cfebe0c54f725ecf5b3cdfc026118f3f795
parag-0501 <parag0501ds@gmail.com>
2023-10-10 22:54:09 +0530

    ans 20 both write and read

---
+++ HandsOn2/20/20b.c
@@ -0,0 +1,46 @@
+/*
+============================================================================
+Name : 20
+Author :Parag Dutt Sharma
+Description : Write two programs so that both can communicate by FIFO -Use one way communication (Reading).
+Date: 10-october-2023
+============================================================================
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main() {
+    // Open the FIFO for reading
+    char *fifoPath = "myfifo";  // Replace with your desired FIFO name
+    int fd = open(fifoPath, O_RDONLY);
+    if (fd == -1) {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+
+    // Buffer to store the received message
+    char buffer[256];
+    ssize_t bytesRead = read(fd, buffer, sizeof(buffer));
+    if (bytesRead == -1) {
+        perror("read");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Null-terminate the received data to treat it as a string
+    buffer[bytesRead] = '\0';
+
+    printf("Message received: %s\n", buffer);
+
+    // Close the FIFO
+    close(fd);
+
+    return 0;
+}
+
