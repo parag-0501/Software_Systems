316135ba32d58449dc9cf23925e1b21989bd3596
parag-0501 <parag0501ds@gmail.com>
2023-10-10 23:02:53 +0530

    ans 21 bot write and read

---
+++ HandsOn2/21/21a.c
@@ -0,0 +1,50 @@
+/*
+============================================================================
+Name : 21
+Author : Parag Dutt Sharma
+Description : Write two programs so that both can communicate by FIFO -Use two way communications. (Writing)
+Date: 10-october-2023
+============================================================================
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define FIFO_PATH "myfifo" // Replace with your desired FIFO name
+
+int main() {
+    int fd;
+    char message[256];
+
+    // Create or open the FIFO for writing
+    mkfifo(FIFO_PATH, 0666);
+    fd = open(FIFO_PATH, O_WRONLY);
+
+    if (fd == -1) {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        printf("Enter a message (or type 'exit' to quit): ");
+        fgets(message, sizeof(message), stdin);
+
+        // Remove newline character from the message
+        message[strlen(message) - 1] = '\0';
+
+	write(fd, message, strlen(message));
+
+        if (strcmp(message, "exit") == 0) {
+            break;
+        }
+    }
+
+    close(fd);
+
+    return 0;
+}
