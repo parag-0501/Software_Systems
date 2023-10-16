a4dcaf9bde58f38760d86c89d57a793596fbbd5d
parag-0501 <parag0501ds@gmail.com>
2023-10-16 19:42:57 +0530

    Updated all and pushed

---
+++ HandsOn2/24/24.c
@@ -0,0 +1,37 @@
+/*
+============================================================================
+ Name : 24.c
+Author : Parag Dutt Sharma
+ Description : Write a program to create a message queue and print the key and message queue id. 
+ Date: 10th OCT, 2023.
+============================================================================
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/msg.h>
+#include <sys/ipc.h>
+
+int main() {
+    int msgqid;
+    key_t key;
+
+    // Generate a unique key for the message queue
+    key = ftok("/tmp", 'A');
+    if (key == -1) {
+        perror("ftok");
+        exit(EXIT_FAILURE);
+    }
+
+    
+    msgqid = msgget(key, 0666 | IPC_CREAT);
+    if (msgqid == -1) {
+        perror("msgget");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Message Queue Key: 0x%x\n", (unsigned int)key);
+    printf("Message Queue ID: %d\n", msgqid);
+
+    return 0;
+}
